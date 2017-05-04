729410d5fbb12923e99ac10dc77ffffa1ef8a36a
ramirezleonel <ramirezleonel0@gmail.com>
2017-05-04 11:17:19 -0300

    cosas en c sharp

---
+++ c#/BusquedaBinaria/BusquedaBinaria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class BusquedaBinaria
+    {
+        public int busqueda(int[]valores,int encontrado) {
+            int primero=0;
+            int ultimo=valores.Length-1;
+            int medio;
+
+            while (primero <= ultimo)
+            {
+                medio = (primero + ultimo) / 2;
+                if (encontrado == valores[medio])
+                    return encontrado;
+                else if (encontrado > valores[medio])
+                {
+                    primero = medio + 1;
+
+                }
+                else if (encontrado < valores[medio])
+                {
+                    ultimo = medio - 1;
+
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ c#/BusquedaBinaria/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elementos = new int[] {1,3,5,6,7,8,9,10,13,15,64,66 };
+            BusquedaBinaria busqueda = new BusquedaBinaria();
+            int buscar=15;
+            if (buscar == busqueda.busqueda(elementos, buscar))
+            {
+                Console.WriteLine("re bien encontraste papa");
+            }
+            else { 
+            
+             Console.WriteLine("no encontraste papa");
+            }
+            Console.ReadKey();
+        }
+    }
+}
